86d9856c75e2ec0493a8a2729f870114ad79aed5
PowWow Admin <admin@studio.dev.powwowinc.net>
2020-05-27 11:50:49 +0000

    [Studio] Delete screen "HomeIconColorBlocks"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,8 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
-import { homeiconcolorblocks_PhonePortrait } from '../pages/homeiconcolorblocks/PhonePortrait/homeiconcolorblocks';
 export class Screens {
   static declarations = [
-    MenuComponent,
-    homeiconcolorblocks_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'homeiconcolorblocks': {
-      PhonePortrait: homeiconcolorblocks_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,2 +1,1 @@
-export { mock_homeiconcolorblocks as homeiconcolorblocks } from './homeiconcolorblocks/index';
 export { mock_menu as menu } from './menu/index';
